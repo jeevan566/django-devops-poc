e460d74b47c97b55588f2f5813c262cc206bcfdf
jeevan566 <jeevan56jeevan56@gmail.com>
2025-09-12 23:14:20 +0000

    feat: added templates and admin registration

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, Order
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "stock")
+    search_fields = ("name",)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at")
+    search_fields = ("user__username",)
